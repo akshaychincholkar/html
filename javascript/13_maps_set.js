f46ac87bce96dc29d38285c8ac2d0ac5de09d0ac
akshaychincholkar <akshaychincholkar@gmail.com>
2024-10-15 01:17:52 +0530

    maps

--- javascript/13_maps_set.js
+++ javascript/13_maps_set.js
@@ -1,13 +1,13 @@
 // limitations of maps
 // 1. There is no complete way to iterate over keys, and the keys() method converts fields to given strings, which leads to the collision of keys.
 // 2.There is also no simple way to add new keys and values to the object.
-const scores = new Map([['Shubham', 12], ['Vaishnavi', 13], ['Juhi', 10], ['Priyanka', 12]]);
-scores.set('sohuard', 14);
+const scores = new Map([['Akshay', 12], ['Darshana', 13], ['Subhash', 10], ['Nita', 12]]);
+scores.set('Avighna', 14);
 console.log(scores.size); // added to the map
 
 // iterations in map
-const scores1 = new Map([['Shubham', 12], ['Vaishnavi', 13], ['Juhi', 10], ['Priyanka', 12]]);
-scores1.set('Sohuard', 14);
+const scores1 = new Map([['Akshay', 12], ['Darshana', 13], ['Subhash', 10], ['Nita', 12]]);
+scores1.set('Avighna', 14);
 console.log(scores1.size);
 for(const [name, score] of scores1.entries()) 
 {
@@ -16,9 +16,9 @@ for(const [name, score] of scores1.entries())
 
 // Converting the keys to arrays
 let userRoles = new Map([
-    ['shubham', 'admin'],
-    ['Vaishnavi', 'editor'],
-    ['Juhi', 'subscriber']
+    ['Akshay', 'admin'],
+    ['Darshana', 'editor'],
+    ['Avighna', 'subscriber']
 ]);
 var keys = [...userRoles.keys()];
 console.log(keys);
