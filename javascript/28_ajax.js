251cf4f204958412b6d71dcf5a4a53528eaf40bb
akshaychincholkar <akshaychincholkar@gmail.com>
2024-10-19 17:39:11 +0530

    Ajax in js

---
+++ javascript/28_ajax.js
@@ -0,0 +1,11 @@
+// AJAX is the combination of:
+
+// A browser built-in XMLHttpRequest object( to request data from a web server).
+// Javascript and HTML DOM(to display or use the data).
+ 
+
+// The name AJAX is itself a little misleading. XML might be used by AJAX to transport the data with a web
+//  server behind the scenes. This signifies that the web page can be updated without reloading it.
+
+// Another advantage of AJAX is that it helps the web pages to update asynchronously by exchanging the data 
+// with a web server behind the scenes. 
